060684a77667026441e5ae8b05861d3894081139
Iulia Anca <iuliaanca00@gmail.com>
2023-04-26 11:02:56 +0300

    Add a models folder with the functionality for making calls to the API.

---
+++ models/models.js
@@ -0,0 +1,281 @@
+class UsersRepository {
+    constructor() {
+
+    }
+
+    getAllUsers() {
+        // call to an API to load all users
+        //return a list of users
+        const apiURL = "http://localhost:3000/users";
+
+        const usersList = [];
+
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                const users = JSON.parse(this.responseText);
+
+                for (let index = 0; index < users.length; index++) {
+
+                    usersList.push(users[index]);
+                }
+            }
+        }
+
+        xmlHttp.open("GET", apiURL);
+        xmlHttp.send();
+
+        return usersList;
+    }
+
+    saveUser(userData) {
+        //save it to storage
+        //Data will come from UI, html inputs
+        const apiURL = "http://localhost:3000/users";
+
+        const newUser = {
+            username: userData.getUsername(),
+            password: userData.getPassword(),
+            firstName: userData.getFirstName(),
+            lastName: userData.getLastName(),
+            email: userData.getEmail(),
+            description: "empty",
+            isAdmin: userData.getIsAdmin()
+        }
+
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
+                console.log(this.responseText);
+            }
+        }
+
+        xmlHttp.open("POST", `${apiURL}`, true);
+
+        xmlHttp.setRequestHeader('Content-type', 'application/json');
+
+        xmlHttp.send(JSON.stringify(newUser));
+    }
+
+    updateUser(newUsername, newPassword, newFirstName, newLastName, newEmail, newDescription, id) {
+        //Data will come from UI, html inputs
+        const apiURL = "http://localhost:3000/users";
+
+        const newUser = {
+            username: newUsername,
+            password: newPassword,
+            firstName: newFirstName,
+            lastName: newLastName,
+            email: newEmail,
+            description: newDescription,
+            isAdmin: "false"
+        }
+
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                console.log(this.responseText);
+            }
+        }
+
+        xmlHttp.open("PUT", `${apiURL}/${id}`, true);
+
+        xmlHttp.setRequestHeader('Content-type', 'application/json');
+
+        xmlHttp.send(JSON.stringify(newUser));
+    }
+
+    saveResult(id, testCategory, testDifficulty, testLanguage, testDate, testScore) {
+        //save it to storage
+        //Data will come from UI, html inputs
+        const apiURL = "http://localhost:3000/results";
+
+        const newResult = {
+            userID: id,
+            category: testCategory,
+            difficulty: testDifficulty,
+            language: testLanguage,
+            date: testDate,
+            score: testScore
+        }
+
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
+                console.log(this.responseText);
+            }
+        }
+
+        xmlHttp.open("POST", `${apiURL}`, true);
+
+        xmlHttp.setRequestHeader('Content-type', 'application/json');
+
+        xmlHttp.send(JSON.stringify(newResult));
+    }
+
+    getResultByUserId(id) {
+        // call to an API to load all users
+        //return a list of users
+        const apiURL = "http://localhost:3000/results";
+
+        const resultsList = [];
+
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                const results = JSON.parse(this.responseText);
+
+                for (let index = 0; index < results.length; index++) {
+
+                    if (results[index].userID === id) {
+                        resultsList.push(results[index]);
+                    }
+                }
+            }
+        }
+
+        xmlHttp.open("GET", apiURL);
+        xmlHttp.send();
+
+        return resultsList;
+    }
+
+    saveUserMessage(id, userFirstName, userLastName, userEmail, userMessage) {
+        const apiURL = "http://localhost:3000/messages";
+
+        const newMessage = {
+            userID: id,
+            firstName: userFirstName,
+            lastName: userLastName,
+            email: userEmail,
+            message: userMessage
+        }
+
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 201) {
+                console.log(this.responseText);
+            }
+        }
+
+        xmlHttp.open("POST", `${apiURL}`, true);
+
+        xmlHttp.setRequestHeader('Content-type', 'application/json');
+
+        xmlHttp.send(JSON.stringify(newMessage));
+    }
+}
+
+
+class QuestionsRepository {
+    constructor() {
+
+    }
+
+    getAllQuestionsEn() {
+        // call to an API to load all questions
+        //return a list of questions
+        const apiURL = "  http://localhost:3000/questions";
+
+        const questionList = [];
+
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                const questions = JSON.parse(this.responseText);
+
+                for (let index = 0; index < questions.length; index++) {
+
+                    questionList.push(questions[index]);
+                }
+            }
+        }
+
+        xmlHttp.open("GET", apiURL);
+        xmlHttp.send();
+
+        return questionList;
+    }
+
+    getTest(category, difficulty, language) {
+        // call to an API to load all questions
+        //return a list of questions
+        const apiURL = "  http://localhost:3000/questions";
+
+        const questionList = [];
+
+        const xmlHttp = new XMLHttpRequest();
+
+        xmlHttp.onreadystatechange = function () {
+            if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                const questions = JSON.parse(this.responseText);
+
+                for (let index = 0; index < questions.length; index++) {
+
+                    if(questions[index].category === category && questions[index].difficulty === difficulty && questions[index].language === language) {
+                        questionList.push(questions[index]);
+                    }
+                }
+            }
+        }
+
+        xmlHttp.open("GET", apiURL);
+        xmlHttp.send();
+
+        return questionList;
+    }
+
+}
+
+class User {
+    #username;
+    #password;
+    #firstName;
+    #lastName;
+    #email;
+    #isAdmin;
+
+    constructor(username, password, firstName, lastName, email, isAdmin) {
+        this.#username = username;
+        this.#password = password;
+        this.#firstName = firstName;
+        this.#lastName = lastName;
+        this.#email = email;
+        this.#isAdmin = isAdmin;
+    }
+
+    getUserFullName() {
+        return `${this.#firstName} - ${this.#lastName}`;
+    }
+
+    getUsername() {
+        return `${this.#username}`;
+    }
+
+    getPassword() {
+        return `${this.#password}`;
+    }
+
+    getFirstName() {
+        return `${this.#firstName}`;
+    }
+
+    getLastName() {
+        return `${this.#lastName}`;
+    }
+
+    getEmail() {
+        return `${this.#email}`;
+    }
+
+    getIsAdmin() {
+        return `${this.#isAdmin}`;
+    }
+}
